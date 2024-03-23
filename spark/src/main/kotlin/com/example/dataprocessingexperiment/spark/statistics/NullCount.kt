5118844b5b91a93129c175aa8957454bbc76738d
paulrule <paulrule1@gmail.com>
2024-03-24 08:51:34 +1100

    a hopefully more efficient implementation of null count

--- spark/src/main/kotlin/com/example/dataprocessingexperiment/spark/statistics/NullCount.kt
+++ spark/src/main/kotlin/com/example/dataprocessingexperiment/spark/statistics/NullCount.kt
@@ -1,17 +1,19 @@
 package com.example.dataprocessingexperiment.spark.statistics
 
+import com.example.dataprocessingexperiment.spark.ScalaToKotlin
 import com.example.dataprocessingexperiment.spark.data.SparkDataTypes
 import com.example.dataprocessingexperiment.spark.statistics.collectors.Collector
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 import mu.KotlinLogging
+import org.apache.spark.sql.Column
 import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.functions
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.StructField
 
 /**
- * Todo: is there a better way to do this? Current implementation counts nulls for each column one at a time.
- * This wouldn't perform well if data was large. Can it be done without scanning the table for each column?
  */
 @Serializable
 class NullCount(private val columns: List<String>) : Statistic {
@@ -27,19 +29,27 @@ class NullCount(private val columns: List<String>) : Statistic {
             data.columns().toList()
         }
 
-        val fields = data.schema().toList()
-        fields.foreach {
-            if (cols.contains(it.name())) {
-                val type = sparkDataTypes.type(it.dataType())
-                if (type != null) {
-                    val count = data.where(type.nullPredicate.invoke(functions.col(it.name()), it.nullable())).count()
-                    collector.add(
-                        "NullCount", it.name(), "", count
+        val fields: List<StructField> = ScalaToKotlin.toList(data.schema().toList())
+
+        // columns we want to select with null count criteria
+        val colsToSelect: List<Column> = fields.filter { cols.contains(it.name()) }
+            .filter { sparkDataTypes.type(it.dataType()) != null }
+            .map {
+                functions.count_if(
+                    sparkDataTypes.type(it.dataType())?.nullPredicate?.invoke(
+                        col(it.name()),
+                        it.nullable()
                     )
-                } else {
-                    logger.warn { "Skipping null count for ${it.name()} because type could not be determined" }
-                }
+                ).alias(it.name())
             }
+
+        // result set contains null count for each column in a column of the same name
+        val result = data.select(*colsToSelect.map { it }.toTypedArray())
+
+        fields.forEach {
+            collector.add(
+                "NullCount", it.name(), "", result.select(it.name()).first()[0]
+            )
         }
 
     }
