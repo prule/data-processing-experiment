b3776dd6490330733589bac406ade72158465cb0
paulrule <paulrule1@gmail.com>
2024-06-15 05:13:45 +1000

    dependency improvements via libs.versions.toml

--- tables/build.gradle.kts
+++ tables/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     alias(libs.plugins.jvm)
 
-    kotlin("plugin.serialization") version "1.9.20"
-    id("org.jetbrains.dokka") version "1.9.10"
+    kotlin("plugin.serialization") version "${libs.plugins.jvm.get().version}"
+    id("org.jetbrains.dokka") version "${libs.plugins.jvm.get().version}"
 
 }
 
@@ -16,22 +16,10 @@ repositories {
 }
 
 dependencies {
-
-    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation(libs.bundles.logging)
     implementation("io.github.xn32:json5k:0.3.0")
 
-
-    // Use the Kotlin JUnit 5 integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
-    // Use the JUnit 5 integration.
-    testImplementation(libs.junit.jupiter.engine)
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    testImplementation ("io.kotest:kotest-runner-junit5:5.8.0")
-    testImplementation ("io.kotest:kotest-assertions-core:5.8.0")
+    testImplementation(libs.bundles.kotest)
 
 }
 
